37f840807203f6af74dd5a8e73aee3c93618668e
binbenban <bin.ben.ban@gmail.com>
2023-11-16 09:35:07 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
