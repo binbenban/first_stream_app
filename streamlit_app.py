781ed5cec38702c8ab2f5af2b15011c4952948c0
binbenban <bin.ben.ban@gmail.com>
2023-11-16 22:11:55 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,6 @@ st.subheader('Raw data')
 st.write(data)
 
 st.subheader('number of pickups by hour')
-hist_values = np.histogram(data[DATA_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
 
 st.bar_chart(hist_values)
